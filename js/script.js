001858daf0c3102d52b6e34957afb97c2aa46af8
Nabed Khan <nabed420@gmail.com>
2020-07-19 21:19:39 +0600

    website upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+  
+  // owl-carousel plugin activation
+  $(".slider-list").owlCarousel({
+	items: 1,
+  	dots: true,
+  	rtl: false,
+  	autoplay:true,
+  });
+
+  // Masonry plugin activation
+  $('.masonary').masonry({
+  });
+
+});
